c9a0154aeb4909be4cf7fc09f7663e021eeb403c
xuerongNanopay <xuerong@nanopay.net>
2025-04-27 12:34:38 -0400

    save change

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -5,17 +5,28 @@ pub struct Subject {
     pub subject_type: String,
 }
 
+pub struct SubjectRole {
+    pub subject_id: u64,
+    pub role_id: String,
+}
+
 pub struct Role {
-    pub id: u64,
+    pub id: String,
     pub name: String,
     pub description: Option<String>,
 }
 
+pub struct RolePermission {
+    pub role_id: String,
+    pub permission_id: String,
+}
+
 pub struct Permission {
-    pub id: u64,
+    pub id: String,
     pub resource: String,
     pub action: u16, //TODO: use bitmap for permission.
 }
+
 
 impl Permission {
     pub const READ:   u16 = 1<<0;
@@ -26,23 +37,35 @@ impl Permission {
     const W_MASK: u16 = Self::UPDATE | Self::DELETE | Self::CREATE;
     const RW_MASK: u16 = Self::W_MASK | Self::READ;
 
+    #[inline]
     pub fn is_readonly(&self) -> bool {
         self.action & Self::RW_MASK == Self::READ
     }
 
+    #[inline]
     pub fn is_write(&self) -> bool {
         self.action & Self::W_MASK == Self::W_MASK
     }
-}
 
-pub struct SubjectRole {
-    pub subject_id: u64,
-    pub role_id: u64,
-}
+    #[inline]
+    pub fn is_create(&self) -> bool {
+        self.action & Self::CREATE == Self::CREATE
+    }
 
-pub struct RolePermission {
-    pub role_id: u64,
-    pub permission_id: u64,
+    #[inline]
+    pub fn is_delete(&self) -> bool {
+        self.action & Self::DELETE == Self::DELETE
+    }
+
+    #[inline]
+    pub fn is_update(&self) -> bool {
+        self.action & Self::UPDATE == Self::UPDATE
+    }
+
+    #[inline]
+    pub fn is_create_and_update(&self) -> bool {
+        self.is_create() && self.is_update()
+    }
 }
 
 pub struct PermissionStore {
